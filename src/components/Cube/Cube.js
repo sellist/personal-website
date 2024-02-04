48b48c6f66acc395df744bf168d20ec6295accba
Scott Ellis <scottellis94@gmail.com>
2024-02-04 00:29:47 -0500

    add hello message

--- src/components/Cube/Cube.js
+++ src/components/Cube/Cube.js
@@ -5,12 +5,12 @@ const Cube = () => {
     return (
     <div class="container">
         <div class="cube cube1">
-            <div class="cube-face cube-face--front"></div>
-            <div class="cube-face cube-face--back"></div>
-            <div class="cube-face cube-face--left"></div>
-            <div class="cube-face cube-face--right"></div>
-            <div class="cube-face cube-face--top"></div>
-            <div class="cube-face cube-face--bottom"></div>
+            <div class="cube-face cube-face--front">Hello!</div>
+            <div class="cube-face cube-face--back">Hello!</div>
+            <div class="cube-face cube-face--left">Hello!</div>
+            <div class="cube-face cube-face--right">Hello!</div>
+            <div class="cube-face cube-face--top">Hello!</div>
+            <div class="cube-face cube-face--bottom">Hello!</div>
         </div>
     </div>
     )
