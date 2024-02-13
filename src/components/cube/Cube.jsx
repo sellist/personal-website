a4b9991dfe77ed48efca4445ea2073e3a134202c
Scott Ellis <scottellis94@gmail.com>
2024-02-13 00:03:53 -0500

    try again

--- src/components/cube/Cube.jsx
+++ src/components/cube/Cube.jsx
@@ -1,20 +1,17 @@
-
 import './styles.css'
 
 function Cube() {
     return (
-        <>
-            <div className="container">
-                <div className="cube cube1">
-                    <div className="cube-face cube-face--front"></div>
-                    <div className="cube-face cube-face--back"></div>
-                    <div className="cube-face cube-face--left"></div>
-                    <div className="cube-face cube-face--right"></div>
-                    <div className="cube-face cube-face--top"></div>
-                    <div className="cube-face cube-face--bottom"></div>
-                </div>
-            </div>
-    </>
+    <div className="container">
+        <div className="cube cube1">
+            <div className="cube-face cube-face--front"></div>
+            <div className="cube-face cube-face--back"></div>
+            <div className="cube-face cube-face--left"></div>
+            <div className="cube-face cube-face--right"></div>
+            <div className="cube-face cube-face--top"></div>
+            <div className="cube-face cube-face--bottom"></div>
+        </div>
+    </div>
     )
 }
 
