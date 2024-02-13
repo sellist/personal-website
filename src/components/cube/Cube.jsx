ddd2903134dfcfc98fb5e05c643aabefa8c559c5
Scott Ellis <scottellis94@gmail.com>
2024-02-12 23:52:42 -0500

    add cube placeholder

---
+++ src/components/cube/Cube.jsx
@@ -0,0 +1,21 @@
+
+import './styles.css'
+
+function Cube() {
+    return (
+        <>
+            <div className="container">
+                <div className="cube cube1">
+                    <div className="cube-face cube-face--front"></div>
+                    <div className="cube-face cube-face--back"></div>
+                    <div className="cube-face cube-face--left"></div>
+                    <div className="cube-face cube-face--right"></div>
+                    <div className="cube-face cube-face--top"></div>
+                    <div className="cube-face cube-face--bottom"></div>
+                </div>
+            </div>
+    </>
+    )
+}
+
+export default Cube
